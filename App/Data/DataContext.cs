5cf0280ad97b580fbd44517728878a1169c561a0
Sebastian Romedahl <sebastian.romedahl@iths.se>
2021-05-27 17:29:01 +0200

    Created database from code

--- App/Data/DataContext.cs
+++ App/Data/DataContext.cs
@@ -1,8 +1,10 @@
+using App.Entities;
 using Microsoft.EntityFrameworkCore;
 namespace App.Data
 {
     public class DataContext : DbContext
     {
+        public DbSet<Course> Courses { get; set; }
         public DataContext(DbContextOptions options) : base(options)
         {
             
---
+++ App/Data/Migrations/20210527152130_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Length = table.Column<string>(type: "TEXT", nullable: true),
+                    Category = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
